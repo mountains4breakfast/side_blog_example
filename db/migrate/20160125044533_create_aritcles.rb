50161782373b46b58fbe1b9d9df546a71489f4f8
Seth E <savethewhales77@gmail.com>
2016-02-01 13:49:38 -0700

    blog commit

---
+++ db/migrate/20160125044533_create_aritcles.rb
@@ -0,0 +1,10 @@
+class CreateAritcles < ActiveRecord::Migration
+  def change
+    create_table :articles do |t|
+      t.string :title
+      t.text :text
+
+      t.timestamps null: false
+    end
+  end
+end
